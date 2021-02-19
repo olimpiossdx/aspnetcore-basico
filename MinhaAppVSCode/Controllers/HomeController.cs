4d07d99cedb2744274e304887b7dd1f5e0d29c1e
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-18 22:33:24 -0300

    Criando primeiro exemplo de web-mvc-controller e alterando os appsettings

---
+++ MinhaAppVSCode/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaAppVSCode.Models;
+
+// para rodar aplicação de acordo com envairment
+// dotnet new --launch-profile Pord  ou QA, ou DEV
+namespace MinhaAppVSCode.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}

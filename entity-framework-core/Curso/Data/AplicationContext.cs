ea147cd6a392a64f613a0733ec48a7223643f577
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-10-07 17:16:36 -0300

    Inserindo registros em massa

--- entity-framework-core/Curso/Data/AplicationContext.cs
+++ entity-framework-core/Curso/Data/AplicationContext.cs
@@ -1,17 +1,23 @@
 using CursoEFCore.Data.Configurations;
 using CursoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CursoEFCore.Data
 {
   public class AplicationContext : DbContext
   {
+    private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
     public DbSet<Pedido> Pedidos { get; set; }
     public DbSet<Produto> Produtos { get; set; }
     public DbSet<Cliente> Clientes { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
     {
-      dbContextOptionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEFCore;Integrated Security=true");
+      dbContextOptionsBuilder
+        .UseLoggerFactory(_logger)
+        .EnableSensitiveDataLogging()
+        .UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEFCore;Integrated Security=true");
+
       base.OnConfiguring(dbContextOptionsBuilder);
     }
 
--- entity-framework-core/Curso/Program.cs
+++ entity-framework-core/Curso/Program.cs
@@ -20,8 +20,38 @@ namespace CursoEFCore
       }
 
       Console.WriteLine("Aplicação iniciada");
-      InserirDados();
+      InserirDadosEmMassa();
     }
+
+    private static void InserirDadosEmMassa()
+    {
+      var produto = new Produto
+      {
+        Descricao = "Produto teste",
+        CodigoBarras = "12234567891358",
+        Valor = 10m,
+        TipoProduto = ValueObjects.TipoProduto.MercadoriaParaRevenda,
+        Ativo = true
+      };
+
+      var cliente = new Cliente
+      {
+        Nome = "Jujé",
+        CEP = "9899999",
+        Cidade = "jujéCidade",
+        Estado = "JJ",
+        Telefone = "9855555555"
+      };
+
+      using var db = new Data.AplicationContext();
+
+      db.AddRange(produto, cliente);
+      var registros = db.SaveChanges();
+
+      Console.WriteLine($"Total registros(s) :{registros}");
+
+    }
+
     private static void InserirDados()
     {
       var produto = new Produto

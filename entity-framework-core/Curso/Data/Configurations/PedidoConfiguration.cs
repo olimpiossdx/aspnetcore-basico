7f37b3ac21b831106c418a2e778308e1d3b52a44
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-10-07 15:20:31 -0300

    Correção de configuration

--- entity-framework-core/Curso/Data/Configurations/PedidoConfiguration.cs
+++ entity-framework-core/Curso/Data/Configurations/PedidoConfiguration.cs
@@ -10,7 +10,7 @@ namespace CursoEFCore.Data.Configurations
     {
       builder.ToTable("Pedidos");
       builder.HasKey(p => p.Id);
-      builder.Property(p => p.InciadoEm).HasDefaultValue("GETDATE()").ValueGeneratedOnAdd();
+      builder.Property(p => p.InciadoEm).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd();
       builder.Property(p => p.Status).HasConversion<string>();
       builder.Property(p => p.TipoFrete).HasConversion<int>();
       builder.Property(p => p.Observacao).HasColumnType("VARCHAR(512)");
